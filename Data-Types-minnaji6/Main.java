79c697bef7d45e2296e95dda9253f6f610f31d8e
Tosie <minnaji@outlook.com>
2022-10-04 16:08:38 -0400

    data types

---
+++ Data-Types-minnaji6/Main.java
@@ -0,0 +1,49 @@
+class Main {
+  public static void main(String[] args) {
+    // Place your code within this method
+    byte by = 127;
+    System.out.println(by);
+    byte by2 = -128;
+    System.out.println(by2);
+
+    short s = 32_767;
+    System.out.println(s);
+    short s2 = -32_768;
+    System.out.println(s2);
+
+    int i = 2_147_483_647;
+    System.out.println(i);
+    int i2 = -2_147_483_648;
+    System.out.println(i2);
+
+    long l = 9_223_372_036_854_775_807L;
+    System.out.println(l);
+
+    double d = 3839.34839;
+    System.out.println(d);
+
+    float f = 3839.34839F;
+    System.out.println(f);
+    float f2 = 3.14F;
+    System.out.println(f2);
+
+    boolean on = false;
+    System.out.println(on);
+
+    boolean myBoolean = true;
+    System.out.println(myBoolean);
+
+    char c = 'a';
+    System.out.println(c);
+
+    char zad = 'z';
+    System.out.println(zad);
+
+    String sentence = "the quick brown fox jumps over the lazy dog";
+    System.out.println(sentence);
+
+    String mySentence = "I mastered Java data types!";
+    System.out.println(mySentence);
+
+  } // End of Main method
+} // End of Main class
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RevUp
+Learning Java
